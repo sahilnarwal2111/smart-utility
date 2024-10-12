0a5bd864e41142d045f26c64e14ccdef7f777014
Harshit Pandey <harshgamingpb@gmail.com>
2024-10-12 22:44:31 +0530

    Added forget-Password and Reset-Password API for Client

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -6,4 +6,6 @@ router.post("/signup/client",authController.signupClient);
 router.post("/signup/provider",authController.signupProvider);
 router.post("/login/client",authController.loginClient);
 router.post("/login/provider",authController.loginProvider);
+router.post("/forget-password/client",authController.forgetPasswordClient);
+router.post("/reset-password/client",authController.resetPasswordClient);
 module.exports = router;
