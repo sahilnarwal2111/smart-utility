1221094c680e11b747476b8e256e3f2b28430b24
Harshit Pandey <harshgamingpb@gmail.com>
2024-11-18 17:30:22 +0530

    added updation functionality for client and provider

--- backend/controllers/ModifyController.js
+++ backend/controllers/ModifyController.js
@@ -54,3 +54,69 @@ exports.addService = async (req,res) => {
         res.status(500).json({message:"Invalid Server Error"});
     }
 }
+
+exports.updateClient = async (req,res) => {
+    try
+    {
+        const {username , key ,value} = req.body;
+
+        let user = await Client.findOne({username:username});
+
+        if(!user){
+            res.status(400).json({message: " User not Found"})
+            return;
+        }
+        if(key == "username"){
+            return res.status(400).json({message:"username can't be changed"})
+        }
+        if(key == "password"){
+            //hash here not now
+            return res.status(400).json({message:"working on change password functionality"})
+        }
+        else {
+            user[key] = value;
+        }
+        user.save();
+
+        res.status(200).json({
+            message:"Data Updated Successfully"
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({message:"Internal Server Error",err: error.message});
+    }
+}
+
+exports.updateProvider = async (req,res) => {
+    try
+    {
+        const {username , key ,value} = req.body;
+
+        let user = await Provider.findOne({username:username});
+
+        if(!user){
+            res.status(400).json({message: " User not Found"})
+            return;
+        }
+        if(key == "username"){
+            return res.status(400).json({message:"username can't be changed"})
+        }
+        else if(key == "password"){
+            //hash here not now
+            return res.status(400).json({message:"working on change password functionality"})
+        }
+        else {
+            user[key] = value;
+        }
+        user.save();
+
+        res.status(200).json({
+            message:"Data Updated Successfully"
+        })
+    }
+    catch(error)
+    {
+        res.status(500).json({message:"Internal Server Error",err: error.message});
+    }
+}
