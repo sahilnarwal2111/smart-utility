1dd62ab5a926a0f0063df405969206a328b23b03
Harshit Pandey <harshgamingpb@gmail.com>
2024-11-18 20:55:05 +0530

    Corrected provider-with-service

--- backend/routes/ModifyRoutes.js
+++ backend/routes/ModifyRoutes.js
@@ -5,4 +5,6 @@ const ModifyController = require("../controllers/ModifyController");
 router.post("/add-service",ModifyController.addService);
 router.post("/insert-service",ModifyController.insertSerivce)
 router.post("/update-client",ModifyController.updateClient)
+router.post("/update-provider",ModifyController.updateProvider)
+
 module.exports = router;
