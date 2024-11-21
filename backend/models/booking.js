4ee67992141f64739ecc290d9166353236aa7295
Harshit Pandey <harshgamingpb@gmail.com>
2024-11-21 22:24:31 +0530

    Correct booking schema

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -14,6 +14,12 @@ const bookingSchema = new mongoose.Schema({
     type:String,
     required:true
   },
+  date: {
+    type:String
+  },
+  time: {
+    type:String
+  },
   price: {
     type: String
   },
